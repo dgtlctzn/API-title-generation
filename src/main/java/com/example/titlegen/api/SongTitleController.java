f2949e746bcb0f2005fb06a7330ff77592c858a2
Joseph Perry <josephperry720@gmail.com>
2020-12-29 13:56:47 -0500

    startup names post route

--- src/main/java/com/example/titlegen/api/SongTitleController.java
+++ src/main/java/com/example/titlegen/api/SongTitleController.java
@@ -117,62 +117,87 @@ public class SongTitleController {
             String json = gson.toJson(format);
             return new ResponseEntity<>(json, HttpStatus.FORBIDDEN);
         };
-        Document doc = new Document(songTitles.getName());
-        int nounsNum = 0;
-        int verbsNum = 0;
-        int pronounsNum = 0;
-        int determinersNum = 0;
-        int prepositionsNum = 0;
-        for (Sentence sentence : doc.sentences()) {
-            for (int i = 0; i < sentence.length(); i++) {
-                String pos = sentence.posTag(i);
-                if (pos.contains("NN")) {
-                    // save nouns "NN" to noun table
-                    Nouns noun = new Nouns();
-                    noun.setNoun(sentence.word(i));
-                    noun.setType(sentence.posTag(i));
-                    nounDao.save(noun);
-                    nounsNum += 1;
-                } else if (pos.contains("VB")) {
-                    // save verbs "VB" to verb table
-                    Verbs verb = new Verbs();
-                    verb.setVerb(sentence.word(i));
-                    verb.setType(sentence.posTag(i));
-                    verbDao.save(verb);
-                    verbsNum += 1;
-                } else if (pos.contains("PRP$")) {
-                    // save pronouns "PRP" to pronoun table
-                    Pronouns pronoun = new Pronouns();
-                    pronoun.setPronoun(sentence.word(i));
-                    pronounDao.save(pronoun);
-                    pronounsNum += 1;
-                } else if (pos.contains("DT")) {
-                    // save determiners "DT" to determiner table
-                    Determiners determiner = new Determiners();
-                    determiner.setDeterminer(sentence.word(i));
-                    determinerDao.save(determiner);
-                    determinersNum += 1;
-                } else if (pos.contains("IN")) {
-                    // save prepositions "IN" to preposition table
-                    Prepositions preposition = new Prepositions();
-                    preposition.setPreposition(sentence.word(i));
-                    prepositionDao.save(preposition);
-                    prepositionsNum += 1;
+        if (headers.containsKey("type") && headers.get("type").equals("song")) {
+            // title generation for songs
+            Document doc = new Document(songTitles.getName());
+            int nounsNum = 0;
+            int verbsNum = 0;
+            int pronounsNum = 0;
+            int determinersNum = 0;
+            int prepositionsNum = 0;
+            for (Sentence sentence : doc.sentences()) {
+                for (int i = 0; i < sentence.length(); i++) {
+                    String pos = sentence.posTag(i);
+                    if (pos.contains("NN")) {
+                        // save nouns "NN" to noun table
+                        Nouns noun = new Nouns();
+                        noun.setNoun(sentence.word(i));
+                        noun.setType(sentence.posTag(i));
+                        nounDao.save(noun);
+                        nounsNum += 1;
+                    } else if (pos.contains("VB")) {
+                        // save verbs "VB" to verb table
+                        Verbs verb = new Verbs();
+                        verb.setVerb(sentence.word(i));
+                        verb.setType(sentence.posTag(i));
+                        verbDao.save(verb);
+                        verbsNum += 1;
+                    } else if (pos.contains("PRP$")) {
+                        // save pronouns "PRP" to pronoun table
+                        Pronouns pronoun = new Pronouns();
+                        pronoun.setPronoun(sentence.word(i));
+                        pronounDao.save(pronoun);
+                        pronounsNum += 1;
+                    } else if (pos.contains("DT")) {
+                        // save determiners "DT" to determiner table
+                        Determiners determiner = new Determiners();
+                        determiner.setDeterminer(sentence.word(i));
+                        determinerDao.save(determiner);
+                        determinersNum += 1;
+                    } else if (pos.contains("IN")) {
+                        // save prepositions "IN" to preposition table
+                        Prepositions preposition = new Prepositions();
+                        preposition.setPreposition(sentence.word(i));
+                        prepositionDao.save(preposition);
+                        prepositionsNum += 1;
+                    }
                 }
             }
-        }
-        // list of word types added to db
-        List<String> namesSaved = new ArrayList<>();
-        namesSaved.add(nounsNum + " nouns");
-        namesSaved.add(verbsNum + " verbs");
-        namesSaved.add(pronounsNum + " pronouns");
-        namesSaved.add(determinersNum + " determiners");
-        namesSaved.add(prepositionsNum + " prepositions");
+            // list of word types added to db
+            List<String> namesSaved = new ArrayList<>();
+            namesSaved.add(nounsNum + " nouns");
+            namesSaved.add(verbsNum + " verbs");
+            namesSaved.add(pronounsNum + " pronouns");
+            namesSaved.add(determinersNum + " determiners");
+            namesSaved.add(prepositionsNum + " prepositions");
 
-        formatString format = new formatString(false, namesSaved.toArray(), "words saved to database");
-        Gson gson = new Gson();
-        String json = gson.toJson(format);
-        return new ResponseEntity<>(json, HttpStatus.OK);
+            formatString format = new formatString(false, namesSaved.toArray(), "words saved to database");
+            Gson gson = new Gson();
+            String json = gson.toJson(format);
+            return new ResponseEntity<>(json, HttpStatus.OK);
+
+        } else if (headers.containsKey("type") && headers.get("type").equals("startup")) {
+            // title generation for startups
+            int startupVerbNum = 0;
+            Document doc = new Document(songTitles.getName());
+            for (Sentence sentence : doc.sentences()) {
+                for (int i = 0; i < sentence.length(); i++) {
+                    StartupVerbs startupVerb = new StartupVerbs();
+                    startupVerb.setVerb(sentence.word(i));
+                    startupVerbsDao.save(startupVerb);
+                    startupVerbNum += 1;
+                }
+            }
+            formatString format = new formatString(false, null, String.format("%d words saved to database", startupVerbNum));
+            Gson gson = new Gson();
+            String json = gson.toJson(format);
+            return new ResponseEntity<>(json, HttpStatus.OK);
+        } else {
+            formatString format = new formatString(true, null, "title type not specified in header");
+            Gson gson = new Gson();
+            String json = gson.toJson(format);
+            return new ResponseEntity<>(json, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping(produces= MediaType.APPLICATION_JSON_VALUE)
